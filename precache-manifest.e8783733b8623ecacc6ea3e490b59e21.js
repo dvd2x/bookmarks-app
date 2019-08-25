f71d74df9cbcf1604e024ed8cd4aa1f132349dd1
David Piper <black.god1930@gmail.com>
2019-08-25 08:30:47 -0400

    Updates

---
+++ precache-manifest.e8783733b8623ecacc6ea3e490b59e21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c28bac00abebc43c59030e3af20433",
+    "url": "/bookmarks-app/index.html"
+  },
+  {
+    "revision": "eda85c92c46d984745de",
+    "url": "/bookmarks-app/static/css/main.9a852b59.chunk.css"
+  },
+  {
+    "revision": "f5b90b3340742026bb9f",
+    "url": "/bookmarks-app/static/js/2.1b5b8b64.chunk.js"
+  },
+  {
+    "revision": "eda85c92c46d984745de",
+    "url": "/bookmarks-app/static/js/main.d2f13eb3.chunk.js"
+  },
+  {
+    "revision": "29b1c465431dd736194a",
+    "url": "/bookmarks-app/static/js/runtime~main.7ebfcad7.js"
+  }
+]);
